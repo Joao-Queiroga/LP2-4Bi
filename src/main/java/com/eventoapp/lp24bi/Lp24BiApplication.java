ea03d8c81bd1d1ff718600977bfad05bfe5f5fce
Joao-Queiroga <queiroga.joaofcsol@gmail.com>
2022-01-21 10:04:24 -0300

    Initial commit

---
+++ src/main/java/com/eventoapp/lp24bi/Lp24BiApplication.java
@@ -0,0 +1,13 @@
+package com.eventoapp.lp24bi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lp24BiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lp24BiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eventoapp/lp24bi/Lp24BiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventoapp.lp24bi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lp24BiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
